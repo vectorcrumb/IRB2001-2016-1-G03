75f82eea563e05649bee815f24b97f781bf71b27
Lucas Alvarez <llalvarez@uc.cl>
2016-06-28 23:30:59 -0400

    Finished arduino code. Finished Python code. TODO: Implement challenges in Python code

--- Arduino/RobocDuino3/RobocDuino3.ino
+++ Arduino/RobocDuino3/RobocDuino3.ino
@@ -7,21 +7,26 @@
 #define VALOR_ADELANTE 127
 #define VALOR_ATRAS -127
 #define BUF 5
-#define KP 0.5
+#define Kp 0.5
 
 byte incoming[BUF];
 long dir_left = 1, dir_right = 1;
 
-
 void mover(int izq, int der, int dir_izq, int dir_der) {
-  int error = izq + der
+  int error = 0;
+  if (dir_izq != dir_der) {
+    // En un giro, se va a 0 y el ajuste se anula.
+    error = izq + der
+  } else {
+    error = izq - der
+  }
+  int adjust = error * Kp
   int mapizq = map(abs(izq), 0,100, 0,255);
   int mapder = map(abs(der), 0,100, 0,255);
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW); // parte todo en cero.
-  
+  digitalWrite(IN4, LOW);
   if (dir_der == 1){
     digitalWrite(IN1, 0);
     digitalWrite(IN2, 1);
@@ -38,28 +43,9 @@ void mover(int izq, int der, int dir_izq, int dir_der) {
     digitalWrite(IN3, 1);
     digitalWrite(IN4, 0);
   }
-  
-  analogWrite(ENA-20, mapder);
-  analogWrite(ENB, mapizq);  
-}
-void avanzar() {
-  mover(100, 100, 1,1);
-}
-
-void retroceder() {
-  mover(100, 100, 1, 1);
-}
-
-void reloj() {
-  mover(100, 100, 1, 2);
-}
-
-void contrareloj() {
-  mover(100, 100, 2, 1);
-}
-
-void detenerse() {
-  mover(0, 0, 0, 0);
+  // Invertir signos en caso que se acentue el error
+  analogWrite(ENA - adjust, mapder);
+  analogWrite(ENB + adjust, mapizq);  
 }
 
 const byte encoderL = 2;
@@ -77,7 +63,6 @@ void stepR()
 }
 
 void setup() {
-  // put your setup code here, to run once:
   attachInterrupt(digitalPinToInterrupt(encoderL),stepL,CHANGE);
   attachInterrupt(digitalPinToInterrupt(encoderR),stepR,CHANGE);
   Serial1.begin(9600);
@@ -89,19 +74,13 @@ void setup() {
 }
 
 void loop(){
-  // put your main code here, to run repeatedly:
   if (Serial1.available()){
     Serial1.readBytes(incoming, BUF);
     long motor_left = incoming[0];
     long motor_right = incoming[1];
     dir_left = incoming[2];
     dir_right = incoming[3];
-//    if (motor_left>100 or motor_left<-100){
-//      motor_left = 0;
-//    }
-//    if (motor_right>100 or motor_right<-100){
-//      motor_right = 0;
-//    }
+
     //+, - gira a la derecha
     mover(motor_left, motor_right, dir_left, dir_right);
     Serial.print(motor_left);
@@ -114,7 +93,6 @@ void loop(){
     Serial1.flush();
     Serial.flush();
   }
-
   int delta_t = millis() - prev_time;
   // Update encoder speeds
   speed_l = (int) counter_l / delta_t;
